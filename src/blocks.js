bb8ad81548a3759fbf169e548a98b9df566b171a
Gulg <65360617+GulgDev@users.noreply.github.com>
2023-07-10 16:34:46 +0500

    Update blocks.js

--- src/blocks.js
+++ src/blocks.js
@@ -7,3 +7,11 @@ export class Block {
   }
 }
 
+export class RedstoneWire {
+  update(redstone) {
+    redstone.updateBlock(this.x, this.y, this.z + 1);
+    redstone.updateBlock(this.x, this.y, this.z - 1);
+    redstone.updateBlock(this.x + 1, this.y, this.z);
+    redstone.updateBlock(this.x - 1, this.y, this.z);
+  }
+}
